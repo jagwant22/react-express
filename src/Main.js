5b6f708f821f9c05c51694abc1c2f8ca681d6cc9
Jagwant Sehgal <jagwantsehgal@Jagwants-MacBook-Air.local>
2018-03-31 10:24:48 +0530

    from mac

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 import Routes from './routes/routes' ;
 
+<<<<<<< Updated upstream
 export default () => <Routes />;
+=======
+
+export default class Main extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+>>>>>>> Stashed changes
 
