cd679ed4891c9c678bbe2f5b7b613278dc947423
Jagwant Sehgal <sehgal.jagwant@gmail.com>
2018-04-01 15:02:35 +0530

    push

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react';
 
 export default class Dashboard extends Component {
+	constructor(props){
+		// initialize state
+		super(props);
+		this.state = {
+			loggedIn : false
+		};
+
+	}
+
+	check_if_logged_in = () =>{
+		console.log("checking login status");
+		return this.state.loggedIn ;  
+	};
+
+	componentwillMount(){
+		console.log("inside componentwillMount");
+		let check = this.check_if_logged_in();
+		if(check === false){
+			window.location = '/';
+		}
+	}
+
+
+
+
 	render(){
 		return(
 				<div>
